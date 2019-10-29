210bd6a0b4f7ebd8191ac8a3a59c8ecf16a97a02
EricFerguson76 <turtle0888@gmail.com>
2019-10-29 16:58:49 -0500

    Added more events

--- js/index.js
+++ js/index.js
@@ -37,3 +37,43 @@ window.addEventListener('resize', () => {
 window.addEventListener('scroll', () => {
   document.body.style.backgroundColor = '#B0C4DE';
 });
+
+// WHEEL EVENT
+function zoom(event) {
+  event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // Restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
+
+  // Apply scale transform
+  adventure.style.transform = `scale(${scale})`;
+}
+let scale = 1;
+const adventure = document.querySelector('.img-fluid');
+adventure.onwheel = zoom;
+
+adventure.addEventListener('wheel', zoom);
+
+// MOUSEOVER AND MOUSEOUT
+const destination1 = document.querySelector('.destination p');
+
+destination1.addEventListener('mouseover', () => {
+  destination1.style.transform = 'scale(1.3)';
+  destination1.style.transition = 'all 0.4s';
+});
+
+destination1.addEventListener('mouseout', () => {
+  destination1.style.transform = 'scale(1.0)';
+  destination1.style.transition = 'all 0.4s';
+});
+
+// FOCUS
+const navSection = document.querySelectorAll('a');
+
+navSection.forEach(aSection => {
+  aSection.addEventListener('focus', () => {
+    aSection.style.background = 'orange';
+  });
+});
