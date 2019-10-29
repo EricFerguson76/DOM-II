2d8a126af8d5b24c544d499f570497da58d73cd1
EricFerguson76 <turtle0888@gmail.com>
2019-10-29 15:10:14 -0500

    Added events to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+//DOUBLE CLICK EVENT
+
+const changeImg = document.querySelector('img');
+changeImg.addEventListener('dblclick', () => {
+  changeImg.src = 'img/beachbus.jpg';
+});
+
+// MOUSEENTER AND MOUSELEAVE EVENTS
+const funImage = document.querySelector('.content-destination img');
+
+funImage.addEventListener('mouseenter', () => {
+  funImage.style.transform = 'scale(1.3)';
+  funImage.style.transition = 'all 0.4s';
+});
+
+funImage.addEventListener('mouseleave', () => {
+  funImage.style.transform = 'scale(1.0)';
+  funImage.style.transition = 'all 0.4s';
+});
+
+// CLICK EVENT
+document.querySelectorAll('.text-content').forEach(el => {
+  el.addEventListener('click', () => {
+    el.style.color = '#00BFFF';
+  });
+});
